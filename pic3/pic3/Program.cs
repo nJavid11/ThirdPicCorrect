91806052ac88d143829fd98a257178f3c2713b46
Namazov <tu201803071@aztu.local>
2021-11-01 08:46:12 +0400

    Correction to the task in 3rd picture

---
+++ pic3/pic3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace pic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sizes s = new Sizes()
+            {
+                width = 2,
+                length = 3,
+                height = 1
+
+            };
+
+            Console.WriteLine("Volume of the given figure = " + s.VolumeOfBox(s));
+        }
+    }
+}
---
+++ pic3/pic3/Sizes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pic3
+{
+    class Sizes
+    {
+        public decimal width;
+        public decimal length;
+        public decimal height;
+        public decimal VolumeOfBox(Sizes size)
+        {
+            return size.width * size.length * size.height;
+        }
+    }
+}
